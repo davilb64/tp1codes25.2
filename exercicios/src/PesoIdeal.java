da36d72f6451c7c9c979dc3ded531904a32aac9a
davilb64 <davilopesbrito64@gmail.com>
2025-08-30 11:19:49 -0300

    Inicio exercicios

---
+++ exercicios/src/PesoIdeal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PesoIdeal {
+    public static void main(String[] args) {
+        double altura;
+        char sexo;
+        Scanner entrada = new Scanner(System.in);
+        altura = entrada.nextDouble();
+        sexo = entrada.nextLine();
+        if (altura <= 0){
+            system.out.println("Entrada inválida!");
+        }
+        else if ((sexo =! 'f') || (sexo =! 'm')) {
+
+        }
+        entrada.close();
+    }
+}
