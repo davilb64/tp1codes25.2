7d0d6717cdd2931378d75b17ff7da503e63a9da2
Davi Lopes Brito <davilopesbrito64@gmail.com>
2025-10-01 15:16:21 -0300

    adiciona tarefa 2 design e modelagem

---
+++ exerciciosjava/src/tecnicasdedesign/desenvolvimentodeprojetos/Engenheiro.java
@@ -0,0 +1,13 @@
+package tecnicasdedesign.desenvolvimentodeprojetos;
+
+public class Engenheiro {
+    private final String nome;
+
+    public Engenheiro(String nome, String ignoredCrea) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
